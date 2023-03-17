ce495726a478378203cfe4ac7b2dd813515ff19d
Francisca <117682018+Francieorwa@users.noreply.github.com>
2023-03-17 23:44:47 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :title_is_clickbait
+
+    def title_is_clickbait
+      if title.blank? || !title.include?("Won't Believe") && !title.include?("Secret") && !title.include?("Top") && !title.include?("Guess")
+        errors.add(:title, "must be clickbait-y!")
+      end
+    end
+  end
+  
